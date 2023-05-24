785344368dfb59b0a513a59aabb3abe98991c6ce
root <root@REALISME>
2023-05-24 07:03:20 +0700

    update file

---
+++ pert7/src/app/Http/Controllers/ProductController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $product = Product::orderby('id')->get();
+        $data['product'] = $product;
+        return view('product/index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $data['title'] = 'create product';
+        return view('product/create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            
+        ]);
+
+        $product = new Product([
+            'name' => $request->name,
+            
+        ]);
+        $product->save();
+        return redirect()->route('product.index');
+        // return view('product/index');
+    }
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        $data['product']=$product;
+        return view('product/edit',$data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        $product->update(['name'=>$request->name]);
+        $product->save();
+
+        return redirect()->route('product.index')->with('success', 'Product update successful');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return redirect()->route('product.index')->with('success', 'Product delete successful');
+    }
+}
